e25a8e59e3c68adb599ba4f5d16ee1a8f6597b19
Nicolas <nlarruda@hotmail.com>
2020-11-14 20:40:15 -0300

    cadastro

---
+++ Cliente/cadastro_Clientes.php
@@ -0,0 +1,23 @@
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <script></script>   
+    </head>
+<body>
+    <center>
+    <h1> Cadastro</h1><br><br>
+    </center>
+    <center>
+    <form action="" name="">
+        <p>Nome: <input type="text" name="nome" value=""></p>
+        <p>E-mail: <input type="text" name="email" value=""></p>
+        <p>RG: <input type="text" name="nome" value=""> CPF: <input type="text" name="cpf" value=""></p>
+        <p>Endereço: <input type="text" name="endereco" value=""></p>
+        <p>Telefone: <input type="text" name="telefone" value=""></p>
+        <p>Senha: <input type="password" name="senha" value=""></p>
+        <input type="submit" value="Cadastrar" onload="function cadastrar()">
+    </form>
+    </center>
+</body>
+</html>
