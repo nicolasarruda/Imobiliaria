7a34894b9e8f4715d079d985b3d03d5830e3ea88
Incarnian <fillipe.bonanome@gmail.com>
2020-11-14 21:32:30 -0200

    quack

---
+++ Cliente/contatos.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Imobiliária RELAPSOS</title>
+    <link rel = "stylesheet" href="css/style.css" type = "text/css">
+    <script language="JavaScript" src="js/funcoes.js"></script>
+</head>
+<body>
+    <header class = "header-1">
+        <div class= "info">
+        <ul>
+            <li><img src="imagens\celularbranco.png"></li>
+            <li><p>(11) 9 1234-5678</p></li>
+            <li><img src="imagens\telefonebranco.png"></li>
+            <li><p>(11) 4567-8901</p></li>
+            <li style="margin-left:auto;"></li>
+            <li style = "margin-left: 580px;"><a>|</a></li>
+            <li><img src="imagens\instagram.png"></li>
+            <li style = "font-size: 20px;"><p>imorelapsos</p></li>
+            <li><img src="imagens\facebook.png"></li>
+            <li style = "font-size: 20px;"><p>imorelapsos</p></li>
+            
+            <li style = "margin-left: auto;"><a href = "index.html"><img src="imagens\perfil.png"></a></li>
+            <li style = "font-size: 20px;font-weight: bold;"><a href="index.html">Login</a></li>
+    </ul>
+    </div>
+    </header>
+    <div class = "header-2">
+            <ul>
+            <li><img src="imagens\relapsoslogo.png"></li>
+            <li style = "margin-left:200px;"><a href = "index.php">HOME</a></li>
+            <li><a href = "index.php">IMÓVEIS</a></li>
+            <li><a href = "index.php">CADASTRO DE USUÁRIOS</a></li>
+            <li><a href = "index.php">CONTATOS</a></li>
+            </ul>
+    </div>
+    <hr style = "margin:1px;border: 1px solid silver">
+</body>
+    
+</html>
